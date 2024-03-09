9db949c8b3833b5345cd13278f27ebbfb4724014
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2024-03-09 23:11:20 +0100

    created affect of scaling and added progress view to splash screen.

--- Sunset/Sunset/Screens/Splash/SplashScreen.swift
+++ Sunset/Sunset/Screens/Splash/SplashScreen.swift
@@ -10,21 +10,32 @@ import SwiftUI
 struct SplashScreen: View {
     @Environment(SunsetAppViewModel.self) var viewModel
     @State private var scale = 1.0
+    @State var showProgressView: Bool = false
 
     //TODO: Create Splash screen
     var body: some View {
-        Text("Hello, Splash!")
-            .scaleEffect(scale)
-            .onAppear {
-                withAnimation(.smooth) {
-                    self.scale = 1.25
+        VStack {
+            Image(.sun)
+                .scaleEffect(scale)
+                .onAppear {
+                    withAnimation(.smooth) {
+                        self.scale = 1.25
+                    }
                 }
+                .task {
+                    try? await Task.sleep(for: .seconds(1.5))
+                    showProgressView = true
+                    viewModel.isUserLoggedIn()
+                    try? await Task.sleep(for: .seconds(0.5))
+                    showProgressView = false
+                }
+            if showProgressView {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .tint(.primaryBackgroundLogin)
+                    .padding(.top, 48)
             }
-            .task {
-                try? await Task.sleep(for: .seconds(1.5))
-                viewModel.isUserLoggedIn()
-                try? await Task.sleep(for: .seconds(0.5))
-            }
+        }
     }
 }
 
