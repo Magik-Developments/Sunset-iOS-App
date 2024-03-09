5cb366a010c3edfe4a8f86a2b9d6d7b5b3f837b5
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2024-03-09 19:35:44 +0100

    UI options of signup added in login sheet, also added custom toggle switch.

--- Sunset/Sunset/Screens/Welcome/Views/WelcomeLoginFormView.swift
+++ Sunset/Sunset/Screens/Welcome/Views/WelcomeLoginFormView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 
 struct WelcomeLoginFormView: View {
     @EnvironmentObject var viewModel: WelcomeViewModel
-    
+    @State var isLoginForm: Bool = true
     //TO MOVE TO VM.
     @State var email: String = ""
     @State var password: String = ""
     @FocusState private var isEmailFocused: Bool
     @FocusState private var isPasswordFocused: Bool
+    @State var isToggleOn: Bool = false
     //TO MOVE TO VM.
 
     var body: some View {
         VStack {
-            Text("common.login")
+            Text(isLoginForm ? "common.login" : "common.signup")
                 .sunsetFontPrimary(primaryFont: .primaryBold, primarySize: .headlineL)
 
             RoundedRectangle(cornerRadius: 180)
@@ -50,10 +51,39 @@ struct WelcomeLoginFormView: View {
                         .focused($isPasswordFocused)
                 }
 
-            DSButton(title: "common.login", buttonStyle: SunsetButtonStyles.primaryDefault, size: .large) {
+            if !isLoginForm {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 32)
+                        .foregroundStyle(isToggleOn ? .green : .red)
+                        .opacity(0.2)
+
+                    DSToggle(text: "Condiciones vende tu alma bla bla bla bla bla balkbal te compramos todo bla bla bla bla vendelo todo queremos tus datos a mas no poder", isOn: $isToggleOn)
+                        .padding(16)
+                }
+                .frame(maxWidth: 340, maxHeight: 100)
+            }
+
+            DSButton(title: isLoginForm ? "common.login" : "common.signup",
+                     buttonStyle: isLoginForm ? SunsetButtonStyles.primaryDefault : SunsetButtonStyles.secondaryDark ,
+                     size: .large) {
                 //TODO: Validate fields.
             }
             .padding(.top, 16)
+
+            if isLoginForm {
+                Text("signup.invite")
+                    .sunsetFontSecondary(secondaryFont: .secondaryRegular, secondarySize: .bodyL)
+                    .padding(.horizontal, 8)
+                    .padding(.top, 36)
+                    .multilineTextAlignment(.center)
+
+                DSButton(title: "common.signup",
+                         buttonStyle: SunsetButtonStyles.secondaryDark, size: .medium) {
+                    withAnimation {
+                        isLoginForm.toggle()
+                    }
+                }
+            }
         }
         .tint(.primaryBackgroundLogin)
     }
